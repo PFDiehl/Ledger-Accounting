b971c5dbf05b8fa6f64d41dc664528877856562c
unknown <pfd77@pm.me>
2026-07-29 18:42:29 -0400

    Fix Gmail app password

--- src/lib/email.js
+++ src/lib/email.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'notifications.mtledger@gmail.com',
-    pass: 'elpzbegrychrgyke'
+    pass: 'elpz begr ychr gkde'
   }
 });
 
